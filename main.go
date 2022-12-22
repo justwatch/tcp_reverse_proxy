a10db345f6d5c24a33808f2f01dc1cbf6b6715b6
Simon <online.frey@mailbox.org>
2022-12-22 17:59:23 +0100

    Switch to NewSingleHostReverseProxy

--- main.go
+++ main.go
@@ -19,15 +19,15 @@ var cli struct {
 func main() {
 	kong.Parse(&cli)
 
-	targetUrl, err := url.Parse(cli.OriginAddress)
+	origin, err := url.Parse(cli.OriginAddress)
 	if err != nil {
-		log.Fatal("Failed to parse target address", err)
+		log.Fatal("Failed to parse origin address", err)
 	}
-	if targetUrl.Host == "" {
-		log.Fatal("Failed to parse target address, no host found")
+	if origin.Host == "" {
+		log.Fatal("Failed to parse origin address, no host found")
 	}
-	if targetUrl.Scheme == "" {
-		log.Fatal("Failed to parse target address, no scheme found")
+	if origin.Scheme == "" {
+		log.Fatal("Failed to parse origin address, no scheme found")
 	}
 
 	extraHeaders := map[string]string{}
@@ -39,61 +39,19 @@ func main() {
 		extraHeaders[strings.TrimSpace(parts[0])] = strings.TrimSpace(parts[1])
 	}
 
-	targetQuery := targetUrl.RawQuery
-	director := func(req *http.Request) {
-		req.URL.Scheme = targetUrl.Scheme
-		req.URL.Host = targetUrl.Host
-		req.URL.Path, req.URL.RawPath = joinURLPath(targetUrl, req.URL)
-		if targetQuery == "" || req.URL.RawQuery == "" {
-			req.URL.RawQuery = targetQuery + req.URL.RawQuery
-		} else {
-			req.URL.RawQuery = targetQuery + "&" + req.URL.RawQuery
-		}
-		if _, ok := req.Header["User-Agent"]; !ok {
-			// explicitly disable User-Agent so it's not set to default value
-			req.Header.Set("User-Agent", "")
-		}
-		for header, value := range extraHeaders {
-			req.Header.Add(header, value)
+	handler := func(p *httputil.ReverseProxy) func(http.ResponseWriter, *http.Request) {
+		return func(w http.ResponseWriter, r *http.Request) {
+			log.Println(r.URL)
+			r.Host = origin.Host
+			for header, value := range extraHeaders {
+				r.Header.Add(header, value)
+			}
+			p.ServeHTTP(w, r)
 		}
 	}
-	proxy := &httputil.ReverseProxy{Director: director}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		proxy.ServeHTTP(w, r)
-	})
+	proxy := httputil.NewSingleHostReverseProxy(origin)
+	http.HandleFunc("/", handler(proxy))
 
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", cli.ListenPort), nil))
-}
-
-func joinURLPath(a, b *url.URL) (path, rawpath string) {
-	if a.RawPath == "" && b.RawPath == "" {
-		return singleJoiningSlash(a.Path, b.Path), ""
-	}
-	// Same as singleJoiningSlash, but uses EscapedPath to determine
-	// whether a slash should be added
-	apath := a.EscapedPath()
-	bpath := b.EscapedPath()
-
-	aslash := strings.HasSuffix(apath, "/")
-	bslash := strings.HasPrefix(bpath, "/")
-
-	switch {
-	case aslash && bslash:
-		return a.Path + b.Path[1:], apath + bpath[1:]
-	case !aslash && !bslash:
-		return a.Path + "/" + b.Path, apath + "/" + bpath
-	}
-	return a.Path + b.Path, apath + bpath
-}
-func singleJoiningSlash(a, b string) string {
-	aslash := strings.HasSuffix(a, "/")
-	bslash := strings.HasPrefix(b, "/")
-	switch {
-	case aslash && bslash:
-		return a + b[1:]
-	case !aslash && !bslash:
-		return a + "/" + b
-	}
-	return a + b
 }
