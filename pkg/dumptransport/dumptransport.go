2c748c5ece805b1933dc6dd276e89c8c8588c8f7
Simon <online.frey@mailbox.org>
2023-08-07 18:27:09 +0200

    Print response without quotes

--- pkg/dumptransport/dumptransport.go
+++ pkg/dumptransport/dumptransport.go
@@ -13,10 +13,10 @@ func (p *Transport) RoundTrip(req *http.Request) (*http.Response, error) {
 	reqDump, _ := httputil.DumpRequestOut(req, true)
 	resp, err := http.DefaultTransport.RoundTrip(req)
 	if err != nil {
-		log.Printf("REQUEST\n%q\nRESPONSE\nCould not be loaded as of error %q", reqDump, err)
+		log.Printf("REQUEST\n%s\nRESPONSE\nCould not be loaded as of error %q", reqDump, err)
 		return nil, err
 	}
 	respDump, _ := httputil.DumpResponse(resp, true)
-	log.Printf("REQUEST\n%q\nRESPONSE\n%q", reqDump, respDump)
+	log.Printf("REQUEST\n%s\nRESPONSE\n%s", reqDump, respDump)
 	return resp, err
 }
