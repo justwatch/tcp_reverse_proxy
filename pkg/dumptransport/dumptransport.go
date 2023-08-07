5c79388b65af2b15ce0c719f3623a2b900e49f7c
Simon <online.frey@mailbox.org>
2023-08-07 18:25:04 +0200

    Check error before dumping response

--- pkg/dumptransport/dumptransport.go
+++ pkg/dumptransport/dumptransport.go
@@ -12,6 +12,10 @@ type Transport struct {
 func (p *Transport) RoundTrip(req *http.Request) (*http.Response, error) {
 	reqDump, _ := httputil.DumpRequestOut(req, true)
 	resp, err := http.DefaultTransport.RoundTrip(req)
+	if err != nil {
+		log.Printf("REQUEST\n%q\nRESPONSE\nCould not be loaded as of error %q", reqDump, err)
+		return nil, err
+	}
 	respDump, _ := httputil.DumpResponse(resp, true)
 	log.Printf("REQUEST\n%q\nRESPONSE\n%q", reqDump, respDump)
 	return resp, err
